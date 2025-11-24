4fe6222c37000b55e901cf692d71ec6a74218eba
Robert Kalmar <robert.kalmar@nxp.com>
2025-11-24 12:38:26 +0100

    Fix missing copyright in executorch/backends/nxp/tests
    Signed-off-by: Robert Kalmar <robert.kalmar@nxp.com>

--- backends/nxp/tests/test_edge_passes.py
+++ backends/nxp/tests/test_edge_passes.py
@@ -1,3 +1,8 @@
+# Copyright 2025 NXP
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import numpy as np
 from executorch.backends.nxp.backend.ir.converter.node_converters.ops_converters import (
     ViewCopyConverter,
